416c57476ce2c3791277cd9929a25cd691e23ca2
yosdarwin <rahmiatul.rasyiqah@gmail.com>
2025-09-24 18:03:54 +0700

    Rename "Terms & Conditions" to "Not Included" in CarCreate component

--- resources/js/pages/cars/create.tsx
+++ resources/js/pages/cars/create.tsx
@@ -488,7 +488,7 @@ export default function CarCreate() {
                         {/* Terms & Conditions */}
                         <Card>
                             <CardHeader>
-                                <CardTitle>Terms & Conditions</CardTitle>
+                                <CardTitle>Not Included</CardTitle>
                             </CardHeader>
                             <CardContent className="space-y-4">
                                 {data.terms.map((term, index) => (
